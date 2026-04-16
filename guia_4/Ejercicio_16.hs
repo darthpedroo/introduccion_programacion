1a2287749f91a709de218a039d7f62106166ec77
darthpedroo <pedrovillarino06@gmail.com>
2026-04-16 17:47:36 -0300

    ejercicio 16

--- guia_4/Ejercicio_16.hs
+++ guia_4/Ejercicio_16.hs
@@ -28,4 +28,23 @@ menorDivisorAux n i
 menorDivisor::Integer->Integer
 menorDivisor n = menorDivisorAux n 2
 
+sonCoprimosAux::Integer->Integer->Integer->Bool
+sonCoprimosAux n1 n2 i
+    | n1 == 1 || n2 == 1 = True
+    | i > n1 || i > n2 = True
+    | mod n1 i == 0 && mod n2 i == 0 = False
+    | otherwise = sonCoprimosAux n1 n2 (i+1)
 
+sonCoprimos::Integer->Integer->Bool
+sonCoprimos n1 n2 = sonCoprimosAux n1 n2 2
+
+
+
+nEsimoPrimoAux::Integer->Integer->Integer->Integer
+nEsimoPrimoAux n i primoCounter
+    | esPrimo(i) && primoCounter /= n = nEsimoPrimoAux n (i+1) (primoCounter+1)
+    | esPrimo(i) && primoCounter == n = i
+    | otherwise = nEsimoPrimoAux n (i+1) primoCounter
+
+nEsimoPrimo::Integer->Integer
+nEsimoPrimo n = nEsimoPrimoAux n 1 0
