42ce872cb4fcf5187af804a37b80b947bf8b859d
darthpedroo <pedrovillarino06@gmail.com>
2026-08-31 19:35:04 -0300

    ejercicio 16

--- guia_4/Ejercicio_16.hs
+++ guia_4/Ejercicio_16.hs
@@ -9,7 +9,17 @@ menorDivisor :: Integer->Integer
 menorDivisor n = menorDivisorAux n 2 
 
 esPrimo:: Integer->Bool --hilarante definición!!!
+esPrimo 1 = False
 esPrimo n = n == menorDivisor(n)
 
 sonCoprimos::Integer->Integer->Bool
 sonCoprimos x y = not (menorDivisor(x) == menorDivisor(y))
+
+nEsimoPrimoAux::Integer->Integer->Integer --revisar!!
+nEsimoPrimoAux nFinal i  
+        | nFinal == 0 = (i-1)
+        | esPrimo i = nEsimoPrimoAux (nFinal-1) (i+1) 
+        | otherwise = nEsimoPrimoAux nFinal (i+1) 
+
+nEsimoPrimo::Integer->Integer
+nEsimoPrimo n = nEsimoPrimoAux n 1
